858c5e8659de09cf25884d1997c72c2d650fd6ae
Ng Sheng Tat <shengtatng@gmail.com>
2022-02-06 15:41:08 +0800

    Completion of Revision of Code

--- calculatorapp.py
+++ calculatorapp.py
@@ -6,14 +6,15 @@ import subprocess
 
 # everything in tkinter is widgets, creating the root widgets
 root = tk.Tk()
+
+# setting widget title and size
 root.title("Weird Calculator")
 root.geometry('530x580')
 
 # create icon
 root.iconbitmap('calcicon2.ico')
-memorynumber = 0
 
-# Attach backgroun image decoration
+# reading image decoration
 myimg = ImageTk.PhotoImage(Image.open("calcimgbg.png"))
 label = tk.Label(root, image = myimg)
 label.grid(row=0, column=4)
@@ -22,103 +23,91 @@ label.grid(row=0, column=4)
 clearimg = ImageTk.PhotoImage(file="clearbg.png")
 equalimg = ImageTk.PhotoImage(file="equalbg.png")
 
-# setting font
+# setting font style format
 fontfamily = tkFont.Font(family='Times New Roman', size=10, weight=tkFont.BOLD)
 
-# creating input fields widget
+# creating input fields widget (entry widget)
 e = tk.Entry(root, width=60, borderwidth=10)
 e.grid(row=0, column=0, columnspan=4, padx=8, pady=8)
 
+# for the calculation of memory value
+memorynumber = 0
+
+# to get the existing value in the screen and concatenate with the new numbers
 def addnum(number):
     valnow = str(e.get())
     e.delete(0, tk.END)
     e.insert(0, valnow + number)
 
+# add the math functions together with the numbers
 def mathfunc(opr):
     valnow = str(e.get())
     e.delete(0, tk.END)
     e.insert(0, valnow + opr)
 
+# clear everything
 def clearnum():
     global memorynumber
     memorynumber = 0
     e.delete(0, tk.END)
 
-def addopr():
+# to get the existing value in the screen and concatenate with the operators symbols such as +, -, x, /, ^ and dot
+def allopr(oprs):
     screennumber = e.get()
     e.delete(0, tk.END)
-    e.insert(0, str(screennumber) + "+")
+    e.insert(0, str(screennumber) + str(oprs))
 
-def subopr():
-    screennumber = e.get()
-    e.delete(0, tk.END)
-    e.insert(0, str(screennumber) + "-")
-
-def mulopr():
-    screennumber = e.get()
-    e.delete(0, tk.END)
-    e.insert(0, str(screennumber) + "x")
-
-def divopr():
-    screennumber = e.get()
-    e.delete(0, tk.END)
-    e.insert(0, str(screennumber) + "/")
-
-def dotopr():
-     screennumber = e.get()
-     e.delete(0, tk.END)
-     e.insert(0, str(screennumber) + ".")
-
-def powopr():
-    screennumber = e.get()
-    e.delete(0, tk.END)
-    e.insert(0, str(screennumber) + "^")
-
+# handling the complex functions one by one instantly
 def calcnow():
     screennumber = e.get()
-    print(screennumber)
+    # print(screennumber)
     symbollist = []
     for i in range(len(screennumber)-1, -1, -1):
         # print(screennumber[i])
         if screennumber[i] == "^":
             j = i
             power = float(screennumber[i+1:])
-            print(f"power: {power}")
+            # print(f"power: {power}")
             for k in range(i, -1, -1):
-                print(screennumber[k])
+                # print(screennumber[k])
                 if screennumber[k] in ["+", "-", "x", "/"]:
                     val = float(screennumber[k+1:j])
-                    print(f"base: {val}")
+                    # print(f"base: {val}")
                     break
                 if k == 0:
                     val = float(screennumber[k:j])
-                    print(f"base: {val}")
+                    # print(f"base: {val}")
+
             rewritevalue = round(val**power, 5)
-            print(rewritevalue)
+            # print(rewritevalue)
 
     if "cos" in screennumber:
         myindex = screennumber.index("c")
         val = float(screennumber[myindex+3:])
         rewritevalue = str(math.cos(val))
+
     elif "sin" in screennumber:
         myindex = screennumber.index("s")
         val = float(screennumber[myindex+3:])
         rewritevalue = str(math.sin(val))
+
     elif "tan" in screennumber:
         myindex = screennumber.index("t")
         val = float(screennumber[myindex+3:])
         rewritevalue = str(math.tan(val))
+
     elif "EXP" in screennumber:
         myindex = screennumber.index("E")
         val = float(screennumber[myindex+3:])
         rewritevalue = str(math.exp(val))
+
     elif "log" in screennumber:
         myindex = screennumber.index("l")
         val = float(screennumber[myindex+3:])
         rewritevalue = str(math.log(val))
 
     for a in range(len(screennumber)-1, -1, -1):
-        print(screennumber[a])
+        # print(screennumber[a])
         if screennumber[a] in ["+", "-", "x", "/"]:
             putonvalue = str(screennumber[0:a+1]) + str(rewritevalue)
             break
@@ -134,17 +123,20 @@ def calcnow():
     except:
         pass
 
+# run the program once the button is clicked
 def runexcel():
     subprocess.Popen(r"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE")
 
+# only handles all the values seperated by operators sumbols without the name of the mathematical functions
 def equalopr():
     operationlist = ["+", "-", "x", "/"]
     eqnscreen = str(e.get())
-    print(eqnscreen)
+    # print(eqnscreen)
     oprindex = [-1, int(len(eqnscreen))]
-    print(oprindex)
+    # print(oprindex)
 
     for i in range(len(eqnscreen)):
+        # not exponential too large value, and initially it is having sign
         if eqnscreen[i] in operationlist and eqnscreen[i-1] != "e" and i!=0:
             oprindex.append(i)
 
@@ -160,7 +152,7 @@ def equalopr():
         except:
             # valuelist.append(eqnscreen[oprindex[i]+1:oprindex[i+1]])
             pass
-    print(valuelist)
+    # print(valuelist)
 
     del oprindex[0]
     del oprindex[-1]
@@ -192,7 +184,6 @@ widthvar = 10
 heightvar = 4
 
 # define text button
-buclear = tk.Button(root, image=clearimg, width=92, height=85, command=clearnum)
 bu1 = tk.Button(root, text="1", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: addnum("1"))
 bu2 = tk.Button(root, text="2", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar,font=fontfamily, command=lambda: addnum("2"))
 bu3 = tk.Button(root, text="3", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar,font=fontfamily, command=lambda: addnum("3"))
@@ -203,13 +194,15 @@ bu7 = tk.Button(root, text="7", padx=padxvar, pady=padyvar, width=widthvar, heig
 bu8 = tk.Button(root, text="8", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar,font=fontfamily, command=lambda: addnum("8"))
 bu9 = tk.Button(root, text="9", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar,font=fontfamily, command=lambda: addnum("9"))
 bu0 = tk.Button(root, text="0", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar,font=fontfamily, command=lambda: addnum("0"))
-budot = tk.Button(root, text=".", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar,font=fontfamily, command=dotopr)
+budot = tk.Button(root, text=".", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar,font=fontfamily, command=lambda: allopr("."))
+
+# calculator functions
+buclear = tk.Button(root, image=clearimg, width=92, height=85, command=clearnum)
 bueq = tk.Button(root, image=equalimg, width=92, height=85, command=equalopr)
+buref = tk.Button(root, text="\u21BB", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=calcnow)
 
 # define apps button
-buword = tk.Button(root, text="word", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=trial)
 buexcel = tk.Button(root, text="excel", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=runexcel)
-buref = tk.Button(root, text="\u21BB", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=calcnow)
 
 # define functions buttons
 buexp = tk.Button(root, text="EXP", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: mathfunc("EXP"))
@@ -219,11 +212,11 @@ bucos = tk.Button(root, text="cos", padx=padxvar, pady=padyvar, width=widthvar, 
 busin = tk.Button(root, text="sin", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: mathfunc("sin"))
 
 # define operations buttons
-buadd = tk.Button(root, text="+", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=addopr)
-busub = tk.Button(root, text="-", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=subopr)
-bumul = tk.Button(root, text="x", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=mulopr)
-budiv = tk.Button(root, text="/", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=divopr)
-bupow = tk.Button(root, text="power", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=powopr)
+buadd = tk.Button(root, text="+", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: allopr("+"))
+busub = tk.Button(root, text="-", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: allopr("-"))
+bumul = tk.Button(root, text="x", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: allopr("x"))
+budiv = tk.Button(root, text="/", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: allopr("/"))
+bupow = tk.Button(root, text="power", padx=padxvar, pady=padyvar, width=widthvar, height=heightvar, font=fontfamily, command=lambda: allopr("^"))
 
 # put button onto the screen
 buttonlist = [buclear, buref, buexcel, buexp, buadd, bu1, bu2, bu3, buln, busub, bu4, bu5, bu6, busin, bumul, bu7, bu8, bu9, bucos, budiv, bueq, budot, bu0, butan, bupow]
@@ -232,8 +225,6 @@ for j in range(1, 6):
     for i in range(0, 5):
         buttonlist[counter].grid(row=j, column=i)
         counter += 1
-
-mybutton = tk.Button(root, text="Start", padx=20, pady=13, command=trial, fg="black", bg="yellow") # state=tk.DISABLED
 
 # everthing of applications are looping constantly
 root.mainloop()
